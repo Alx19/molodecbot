5f7d88c1cc786628ebd79ca7c0b605e78bcb4e0f
Alx19 <alx19sucharev@gmail.com>
2020-05-24 00:54:22 +0300

    regex changes

--- bot.rb
+++ bot.rb
@@ -7,16 +7,17 @@ redis = Redis.new
 Telegram::Bot::Client.run(TOKEN) do |bot|
   bot.listen do |message|
     case message.text
-    when '/me', %r{^/me@TbI_molodec_bot}
+    when %r{^/me}
       if message.from.username
-        counter = redis.incr(message.from.username)
         name = message.from.username
+        name = '@' + name if name[0] != '@'
+        counter = redis.incr(name)
         bot.api.send_message(chat_id: message.chat.id, text: "Ты молодец, #{name}! Уже #{counter} раз!")
       else
         name = message.from.first_name
         bot.api.send_message(chat_id: message.chat.id, text: "Ты молодец, #{name}! Создай себе telegram username, чтобы я смог посчитать, сколько раз ты был молодцом!")
       end
-    when %r{^/you}, %r{^/you@TbI_molodec_bot}
+    when %r{^/you}
       name = message.text.split(' ').select { |word| word[0] == '@' }.first
       if name
         counter = redis.incr(name)
