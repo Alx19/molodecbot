fea4a97796f30b529d78dbfcf35b1e46e0a02ec0
Alx19 <alx19sucharev@gmail.com>
2020-05-23 23:14:33 +0300

    adding special cases

--- bot.rb
+++ bot.rb
@@ -7,14 +7,23 @@ redis = Redis.new
 Telegram::Bot::Client.run(TOKEN) do |bot|
   bot.listen do |message|
     case message.text
-    when '/me@TbI_molodec_bot'
-      counter = redis.incr(message.from.id)
-      name = message.from.username || message.from.first_name
-      bot.api.send_message(chat_id: message.chat.id, text: "Ты молодец, #{name}! Уже #{counter} раз!")
-    when %r{^/you@TbI_molodec_bot}
+    when '/me', '/me@TbI_molodec_bot'
+      if message.from.username
+        counter = redis.incr(message.from.username)
+        name = message.from.username
+        bot.api.send_message(chat_id: message.chat.id, text: "Ты молодец, #{name}! Уже #{counter} раз!")
+      else
+        name = message.from.first_name
+        bot.api.send_message(chat_id: message.chat.id, text: "Ты молодец, #{name}! Создай себе telegram username, чтобы я смог посчитать, сколько раз ты был молодцом!")
+      end
+    when %r{^/you}, %r{^/you@TbI_molodec_bot}
       name = message.text.split(' ').select { |word| word[0] == '@' }.first
-      counter = redis.incr(message.from.id)
-      bot.api.send_message(chat_id: message.chat.id, text: "Ты молодец, #{name}! Уже #{counter} раз!")
+      if name
+        counter = redis.incr(name)
+        bot.api.send_message(chat_id: message.chat.id, text: "Ты молодец, #{name}! Уже #{counter} раз!")
+      else
+        bot.api.send_message(chat_id: message.chat.id, text: "Не понять, кто же молодец. Используй @username!")
+      end
     end
   end
 end
