dcc784d8490b5261227555cb15eda88fbcf78b6b
Alx19 <alx19sucharev@gmail.com>
2020-05-23 22:41:54 +0300

    adding redis db

--- bot.rb
+++ bot.rb
@@ -2,12 +2,14 @@ require_relative 'config.rb'
 require 'telegram/bot'
 require 'redis'
 
+redis = Redis.new
+
 Telegram::Bot::Client.run(TOKEN) do |bot|
   bot.listen do |message|
     case message.text
-    when '/тымолодец@TbI_molodec_bot'
-      puts message.text
-      #bot.api.send_message(chat_id: message.chat.id, text: "Сиди дома бля, #{message.from.first_name}")
+    when '/me@TbI_molodec_bot'
+      counter = redis.incr(message.from.id)
+      bot.api.send_message(chat_id: message.chat.id, text: "Ты молодец, #{message.from.first_name}! Уже #{counter} раз!")
     end
   end
 end
