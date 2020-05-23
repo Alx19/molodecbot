8c8ad8d1585f259ed1348b76f1472a95d71f6790
Alx19 <alx19sucharev@gmail.com>
2020-05-23 22:58:58 +0300

    adding /you command

--- bot.rb
+++ bot.rb
@@ -9,7 +9,12 @@ Telegram::Bot::Client.run(TOKEN) do |bot|
     case message.text
     when '/me@TbI_molodec_bot'
       counter = redis.incr(message.from.id)
-      bot.api.send_message(chat_id: message.chat.id, text: "Ты молодец, #{message.from.first_name}! Уже #{counter} раз!")
+      name = message.from.username || message.from.first_name
+      bot.api.send_message(chat_id: message.chat.id, text: "Ты молодец, #{name}! Уже #{counter} раз!")
+    when %r{^/you@TbI_molodec_bot}
+      name = message.text.split(' ').select { |word| word[0] == '@' }.first
+      counter = redis.incr(message.from.id)
+      bot.api.send_message(chat_id: message.chat.id, text: "Ты молодец, #{name}! Уже #{counter} раз!")
     end
   end
 end
