6bb4d345de81cf7e50dd4eacc2d9cadc274f94ce
Alx19 <alx19sucharev@gmail.com>
2020-05-24 00:56:43 +0300

    multiple users

--- bot.rb
+++ bot.rb
@@ -18,10 +18,12 @@ Telegram::Bot::Client.run(TOKEN) do |bot|
         bot.api.send_message(chat_id: message.chat.id, text: "Ты молодец, #{name}! Создай себе telegram username, чтобы я смог посчитать, сколько раз ты был молодцом!")
       end
     when %r{^/you}
-      name = message.text.split(' ').select { |word| word[0] == '@' }.first
-      if name
-        counter = redis.incr(name)
-        bot.api.send_message(chat_id: message.chat.id, text: "Ты молодец, #{name}! Уже #{counter} раз!")
+      names = message.text.split(' ').select { |word| word[0] == '@' }.first
+      if names
+        names.each do |name|
+          counter = redis.incr(name)
+          bot.api.send_message(chat_id: message.chat.id, text: "Ты молодец, #{name}! Уже #{counter} раз!")
+        end
       else
         bot.api.send_message(chat_id: message.chat.id, text: "Не понять, кто же молодец. Используй @username!")
       end
